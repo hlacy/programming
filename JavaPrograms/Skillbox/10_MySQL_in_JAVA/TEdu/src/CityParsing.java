e23fa867bb64a8f98fa8723d9d2f87d5ad067405
hlacy <ivanhoegl287@gmail.com>
2022-12-03 16:56:59 +0300

    1.12.2022

--- JavaPrograms/Skillbox/10_MySQL_in_JAVA/TEdu/src/CityParsing.java
+++ JavaPrograms/Skillbox/10_MySQL_in_JAVA/TEdu/src/CityParsing.java
@@ -1,32 +1,53 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class  CityParsing {
 
     private final static String file = "Задача ВС Java Сбер.csv";
 
 
-    private static City parsingLine(String str){
+    private static City parsingLine(String str) {
         String[] data = str.split(";", 6);
         return new City(data[1], data[2], data[3], Long.parseLong(data[4]), data[5]);
     }
 
     public static List<City> parsingCities() {
-        List<City> cityList = new  ArrayList<>();
+        List<City> cityList = new ArrayList<>();
         try {
             Scanner scanner = new Scanner(new File(file));
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 cityList.add(parsingLine(scanner.nextLine()));
             }
             scanner.close();
-        }
-        catch (FileNotFoundException ex){
+        } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
         return cityList;
     }
+
+    //sorting list by name using lambda
+    public static List<City> sortByNameLambda(List<City> cityList) {
+        cityList.sort((c1, c2) -> (c1.getName().compareToIgnoreCase(c2.getName())));
+        return cityList;
+    }
+
+    //sorting list by name using Comparator
+
+    public static List<City> sortByNameComparator(List<City> cityList){
+        Collections.sort(cityList, new Comparator<City>() {
+            @Override
+            public int compare(City c1, City c2) {
+                return c1.getName().compareToIgnoreCase(c2.getName());
+            }
+        });
+        return cityList;
+    }
+
+
+    //sorting by federal district and name using Comparator
+    public static void sortByDistrictAndNameComparator(List<City> cityList){
+        cityList.sort(Comparator.comparing(City::getDistrict).thenComparing(City::getName));
+    }
+
 }
--- JavaPrograms/Skillbox/10_MySQL_in_JAVA/TEdu/src/Main.java
+++ JavaPrograms/Skillbox/10_MySQL_in_JAVA/TEdu/src/Main.java
@@ -2,8 +2,16 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         List<City> cityList = CityParsing.parsingCities();
+
+        List<City> cityListSorted = CityParsing.sortByNameLambda(cityList);
         cityList.forEach(System.out::println);
+
+        System.out.println();
+
+        List<City> cityList3 = CityParsing.parsingCities();
+        CityParsing.sortByDistrictAndNameComparator(cityList3);
+        cityList3.forEach(System.out::println);
     }
 }
