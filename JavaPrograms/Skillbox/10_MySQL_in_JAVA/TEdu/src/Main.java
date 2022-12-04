6c661b973dbad097d3f0b4c330a01645c5d310ae
hlacy <ivanhoegl287@gmail.com>
2022-12-04 18:59:50 +0300

    04.12.2022 3 path. Printing list of cities 5 path. Sorting list by name of cities 5 path. Sorting list by name in region 7 path. Searching max population and displaying it 9 path. Printing list of numbers of cities in region

--- JavaPrograms/Skillbox/10_MySQL_in_JAVA/TEdu/src/Main.java
+++ JavaPrograms/Skillbox/10_MySQL_in_JAVA/TEdu/src/Main.java
@@ -3,17 +3,25 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //3 path. Printing list of cities
         List<City> cityList = CityParsing.parsingCities();
+        cityList.forEach(System.out::println);
 
-//        List<City> cityListSorted = CityParsing.sortByNameLambda(cityList);
-//        cityList.forEach(System.out::println);
-//
-//        System.out.println();
-//
-//        List<City> cityList3 = CityParsing.parsingCities();
-//        CityParsing.sortByDistrictAndNameComparator(cityList3);
-//        cityList3.forEach(System.out::println);
-//        CityParsing.searchMaxPopulation(cityList);
-        CityParsing.searchCitiesInRegion(cityList);
+        //5 path. Sorting list by name of cities
+        List<City> cityListSorted = CityParsing.sortByNameLambda(cityList);
+        cityList.forEach(System.out::println);
+
+        //5 path. Sorting list by name in region
+        List<City> cityList5 = CityParsing.parsingCities();
+        CityParsing.sortByDistrictAndNameComparator(cityList5);
+        cityList5.forEach(System.out::println);
+
+        //7 path. Searching max population and displaying it
+        List<City> cityList7 = CityParsing.parsingCities();
+        CityParsing.searchMaxPopulation(cityList7);
+
+        //9 path. Printing list of numbers of cities in region
+        List<City> cityList9 = CityParsing.parsingCities();
+        CityParsing.searchCitiesInRegion(cityList9);
     }
 }
